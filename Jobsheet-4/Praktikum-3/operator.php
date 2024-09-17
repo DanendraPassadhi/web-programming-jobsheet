e10eb2cc1662d438ee6ae5a7d154b1137e2c3b2b
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2024-09-17 19:48:16 +0700

    add : php file

---
+++ Jobsheet-4/Praktikum-3/operator.php
@@ -0,0 +1,20 @@
+<?php
+
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Hasil = $hasilTambah <br>";
+echo "Hasil = $hasilKurang <br>";
+echo "Hasil = $hasilKali <br>";
+echo "Hasil = $hasilBagi <br>";
+echo "Hasil = $sisaBagi <br>";
+echo "Hasil = $pangkat";
+
+?>
