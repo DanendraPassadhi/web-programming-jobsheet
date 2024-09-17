70573e5607a7ddad7995c5f91a4435370ab71ca4
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2024-09-17 20:05:40 +0700

    feat : add comparison operations using operators

--- Jobsheet-4/Praktikum-3/operator.php
+++ Jobsheet-4/Praktikum-3/operator.php
@@ -15,6 +15,19 @@ echo "Hasil = $hasilKurang <br>";
 echo "Hasil = $hasilKali <br>";
 echo "Hasil = $hasilBagi <br>";
 echo "Hasil = $sisaBagi <br>";
-echo "Hasil = $pangkat";
+echo "Hasil = $pangkat <br><br>";
 
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+echo "Hasil = $hasilSama <br>";
+echo "Hasil = $hasilTidakSama <br>";
+echo "Hasil = $hasilLebihKecil <br>";
+echo "Hasil = $hasilLebihBesar <br>";
+echo "Hasil = $hasilLebihKecilSama <br>";
+echo "Hasil = $hasilLebihBesarSama";
 ?>
