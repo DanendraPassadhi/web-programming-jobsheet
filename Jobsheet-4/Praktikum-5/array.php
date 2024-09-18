ed396c6ebf3995ca09ebd05cc5e584074786825e
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2024-09-18 18:53:59 +0700

    feat : add multidimensional array, karaywanPengalamanLimaTahun

--- Jobsheet-4/Praktikum-5/array.php
+++ Jobsheet-4/Praktikum-5/array.php
@@ -12,4 +12,24 @@ foreach ($nilaiSiswa as $nilai) {
 
 echo "Daftar nilai siswa yang lulus: " . implode(', ', $nilaiLulus);
 
+echo "<br><br>";
+
+$daftarKaryawan = [
+    ['Alice', 7],
+    ['Bob', 3],
+    ['Charlie', 9],
+    ['David', 5],
+    ['Eva', 6],
+];
+
+$karyawanPengalamanLimaTahun = [];
+
+foreach ($daftarKaryawan as $karyawan) {
+    if ($karyawan[1] > 5) {
+        $karyawanPengalamanLimaTahun[] = $karyawan[0]; // Menambahkan nama karyawan dengan pengalaman lebih dari 5 tahun
+    }
+}
+
+echo "Daftar karyawan dengan pengalaman kerja lebih dari 5 tahun: " . implode(', ', $karyawanPengalamanLimaTahun);
+
 ?>
