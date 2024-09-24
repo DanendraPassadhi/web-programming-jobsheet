1d223459b2f060896764c50000008392eb326fd5
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2024-09-24 07:51:19 +0700

    feat : add default value

--- Jobsheet-5/Praktikum-1/fungsi.php
+++ Jobsheet-5/Praktikum-1/fungsi.php
@@ -10,7 +10,7 @@
 // perkenalan();
 
 //membuat fungsi
-function perkenalan($nama, $salam){
+function perkenalan($nama, $salam="Assalamualaikum"){
     echo $salam . ", ";
     echo "Perkenalkan, nama saya " . $nama . "<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
