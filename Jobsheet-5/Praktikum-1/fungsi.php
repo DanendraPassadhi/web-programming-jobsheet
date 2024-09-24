f03d436688e348bc8a057bbe45d6076f442c2210
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2024-09-24 08:04:22 +0700

    feat : change function to hitungUmur

--- Jobsheet-5/Praktikum-1/fungsi.php
+++ Jobsheet-5/Praktikum-1/fungsi.php
@@ -9,22 +9,31 @@
 // perkenalan();
 // perkenalan();
 
+// //membuat fungsi
+// function perkenalan($nama, $salam="Assalamualaikum"){
+//     echo $salam . ", ";
+//     echo "Perkenalkan, nama saya " . $nama . "<br/>";
+//     echo "Senang berkenalan dengan Anda<br/>";
+// }
+
+// //memanggil fungsi yang sudah dibuat
+// perkenalan("Hamdana", "Hallo");
+
+// echo "<hr>";
+
+// $saya = "Danendra";
+// $ucapanSalam = "Selamat pagi";
+
+// //memanggil lagi
+// perkenalan($saya, $ucapanSalam);
+
 //membuat fungsi
-function perkenalan($nama, $salam="Assalamualaikum"){
-    echo $salam . ", ";
-    echo "Perkenalkan, nama saya " . $nama . "<br/>";
-    echo "Senang berkenalan dengan Anda<br/>";
+function hitungUmur($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
 }
 
-//memanggil fungsi yang sudah dibuat
-perkenalan("Hamdana", "Hallo");
-
-echo "<hr>";
-
-$saya = "Danendra";
-$ucapanSalam = "Selamat pagi";
-
-//memanggil lagi
-perkenalan($saya, $ucapanSalam);
+echo "Umur saya adalah " . hitungUmur(2004, 2024) . " tahun"; 
+// isi sesuai dengan tahun lahir kalian
 
 ?>
