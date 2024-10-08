3e2f757e944677830d4f25f507ccfb3ff333be35
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2024-10-08 10:11:45 +0700

    feat : add JQuery script

--- Jobsheet-7/Praktikum-7/form_validasi.php
+++ Jobsheet-7/Praktikum-7/form_validasi.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>Form Input dengan Validasi</title>
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
 </head>
 <body>
     <h1>Form Input dengan Validasi</h1>
@@ -16,5 +17,34 @@
 
         <input type="submit" value="Submit">
     </form>
+
+    <script>
+$(document).ready(function () {
+    $("#myForm").submit(function (event) {
+        var nama = $("#nama").val();
+        var email = $("#email").val();
+        var valid = true;
+
+        if (nama === "") {
+            $("#nama-error").text("Nama harus diisi.");
+            valid = false;
+        } else {
+            $("#nama-error").text("");
+        }
+
+        if (email === "") {
+            $("#email-error").text("Email harus diisi.");
+            valid = false;
+        } else {
+            $("#email-error").text("");
+        }
+
+        if (!valid) {
+            event.preventDefault();
+        }
+        // Menghentikan pengiriman form jika validasi gagal
+    });
+});
+</script>
 </body>
 </html>
