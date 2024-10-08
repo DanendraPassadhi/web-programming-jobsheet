66192a17ffb5a2fbdd707eb349ba8679a4c583d2
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2024-10-08 09:14:40 +0700

    feat : add preg_match using stars

--- Jobsheet-7/Praktikum-5/regex.php
+++ Jobsheet-7/Praktikum-5/regex.php
@@ -26,4 +26,14 @@ $text = 'I like apple pie.';
 $new_text = preg_replace($pattern, $replacement, $text);
 echo $new_text; // Output: "I like banana pie."
 
+echo "<br>";
+
+$pattern = '/go*d/'; // Cocokkan "god", "good", "goood", dll.
+$text = 'god is good.';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
+} else {
+    echo "Tidak ada yang cocok!";
+}
+
 ?>
