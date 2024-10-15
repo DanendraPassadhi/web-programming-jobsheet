ddf8fb14e39efaae37e32bc68ff01d22e0fcf5af
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2024-10-15 08:28:45 +0700

    feat : added code to send files with file(txt, pdf, doc, docx) extensions only, with a maximum size of 3mb

--- Jobsheet-8/Praktikum-1/upload.php
+++ Jobsheet-8/Praktikum-1/upload.php
@@ -1,17 +1,15 @@
 <?php
-if (isset($_POST["submit"])) {
-    $targetdir = "uploads/"; // Direktori tujuan untuk menyimpan file
+if(isset($_POST["submit"])){
+    $targetdir = "uploads/"; //Direktori tujuan untuk menyimpan file
     $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
-    $maxsize = 5*1024*1024; // 5MB
+    $allowedExtensions = array("txt", "pdf", "doc", "docx");
+    $maxsize = 3*1024*1024;
 
-    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
-        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
-            echo "File berhasil diunggah.";
-
-            echo '<img src="' . $targetfile . '" width="200" style="height:auto;">';
+    if(in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
+        if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)){
+            echo "File berhasil diunggah";
         } else {
             echo "Gagal mengunggah file.";
         }
